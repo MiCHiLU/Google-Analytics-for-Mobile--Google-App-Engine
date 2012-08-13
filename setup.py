ad37bb6b72f7512782b36ed3724fed8d8062635e
MiCHiLU <djmchl@gmail.com>
2012-08-13 13:49:22 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name="GAE-Google-Analytics-mobile",
+      version="1.0",
+      description="An implementation of Google Analytics Google App Engine / Python. Useful for tracking Mobile Applications.",
+      url="https://github.com/MiCHiLU/Google-Analytics-for-Mobile--Google-App-Engine",
+      download_url="https://github.com/MiCHiLU/Google-Analytics-for-Mobile--Google-App-Engine/downloads",
+      py_modules=["ga"],
+      )
